34817d75d58cfffc12626b8e93ca4b50814f9b70
bit56789 <bboy5784@gmail.com>
2016-05-18 19:47:15 +0700

    First copy of all files

---
+++ daily_quiz.php
@@ -0,0 +1,265 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Tell Me Yours</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/queries.css">
+    <link rel="stylesheet" href="css/etline-font.css">
+    <link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body id="top">
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <section class="hero">
+        <section class="navigation">
+            <header>
+                <div class="header-content">
+                    <div class="logo"><a href="#"><img src="img/miniicon.png" alt="Sedna logo"></a></div>
+                    <div class="header-nav">
+                        <nav>
+                          <ul class="primary-nav">
+                              <li><a href="#">Future Career</a></li>
+                              <li><a href="guest.php">Daily Life Sugestion</a></li>
+                              <li><a href="#">Magic 8 Ball</a></li>
+                          </ul>
+                          <ul class="member-actions">
+                                <li><a href="index.html" class="btn-white btn-small">Guest User</a></li>
+                          </ul>
+                        </nav>
+                    </div>
+                    <div class="navicon">
+                        <a class="nav-toggle" href="#"><span></span></a>
+                    </div>
+                </div>
+            </header>
+        </section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="hero-content text-center">
+                        <h2>Daily Life Question</h2>
+                        <p>This question will tell you what do you like for you meal.</p><br>
+                        <p>Scoll Down to take the quiz.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="down-arrow floating-arrow"><a href="#"><i class="fa fa-angle-down"></i></a></div>
+    </section>
+<form name="daily_quiz" id="dailyquiz" method="post" action="daily_submit.php"  >
+<filedset>
+    <section class="features section-padding" id="features">
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-7">
+                    <div class="feature-list">
+                        <h3>1. What kind of cooking do you want ?</h3>
+                        <p>How your food will be cook.</p>
+                        <ul class="features-stack">
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q1"
+                                <?php if (isset($gender) && $gender=="a") echo "checked";?>value="a">
+                                    Fried
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q1"
+                                <?php if (isset($gender) && $gender=="b") echo "checked";?>value="b">
+                                    Boiled
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q1"
+                                <?php if (isset($gender) && $gender=="c") echo "checked";?>value="c">
+                                    Stirred
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="device-showcase">
+            <div class="devices">
+                <div class="ipad-wrap wp1"></div>
+            </div>
+        </div>
+        <div class="responsive-feature-img"><img src="img/devices.png" alt="responsive devices"></div>
+    </section>
+    <section class="features-extra section-padding" id="assets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="feature-list">
+                        <div class="feature-list">
+                        <h3>2. What kind of meat do you prefered ?</h3>
+                        <p>Food made with pork, chicken, beef, or seafood</p>
+                        <ul class="features-stack">
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q2"
+                                <?php if (isset($gender) && $gender=="c") echo "checked";?>value="c">
+                                    Pork & Chicken
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q2"
+                                <?php if (isset($gender) && $gender=="b") echo "checked";?>value="b">
+                                    Beef
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q2"
+                                <?php if (isset($gender) && $gender=="a") echo "checked";?>value="a">
+                                    Seafood
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q2"
+                                <?php if (isset($gender) && $gender=="d") echo "checked";?>value="d">
+                                    Without Meat
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="macbook-wrap wp3"></div>
+        <div class="responsive-feature-img"><img src="img/macbook-pro.png" alt="responsive devices"></div>
+    </section>
+    <section class="features section-padding" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-7">
+                    <div class="feature-list">
+                        <h3>3. Do you want your food to be spicy ?</h3>
+                        <p>The flavor of your food will be spicy or may contain chili.</p>
+                        <ul class="features-stack">
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q3"
+                                <?php if (isset($gender) && $gender=="a") echo "checked";?>value="a">
+                                    Spicy
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q3"
+                                <?php if (isset($gender) && $gender=="b") echo "checked";?>value="b">
+                                    Not Spicy
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="device-showcase">
+            <div class="devices">
+                <div class="chili-wrap wp1"></div>
+            </div>
+        </div>
+        <div class="responsive-feature-img"><img src="img/devices.png" alt="responsive devices"></div>
+    </section>
+    <section class="features-extra section-padding" id="assets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="feature-list">
+                        <div class="feature-list">
+                        <h3>4. What combination do you prefered in your food ?</h3>
+                        <p>Combination with rice, noodles, or other (eg. Steak)</p>
+                        <ul class="features-stack">
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q4"
+                                <?php if (isset($gender) && $gender=="b") echo "checked";?>value="b">
+                                    Rice
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q4"
+                                <?php if (isset($gender) && $gender=="a") echo "checked";?>value="a">
+                                    Noodles
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-content">
+                                    <input type="radio" name="q4"
+                                <?php if (isset($gender) && $gender=="c") echo "checked";?>value="c">
+                                    Other
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="noodle-wrap wp3"></div>
+        <div class="responsive-feature-img"><img src="img/macbook-pro.png" alt="responsive devices"></div>
+    </section>
+
+    <section class="hero-strip section-padding">
+        <div class="container">
+            <div class="col-md-12 text-center">
+                <h2>
+                Are you sure about your answer ? </h2>
+                <p>Once you submit your answer, the program will calculate the menu for you.</p>
+                <button class="btn btn-ghost btn-accent btn-large">SUBMIT !</a>
+            </div>
+        </div>
+    </section>
+</fieldset>
+</form>
+
+    
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="bower_components/retina.js/dist/retina.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="bower_components/classie/classie.js"></script>
+    <script src="bower_components/jquery-waypoints/lib/jquery.waypoints.min.js"></script>
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+</body>
+</html>
---
+++ daily_submit.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Sedna - A Free HTML5/CSS3 website by Peter Finlan</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/queries.css">
+    <link rel="stylesheet" href="css/etline-font.css">
+    <link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body id="top">
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <section class="hero2">
+        <section class="navigation">
+            <header>
+                <div class="header-content">
+                    <div class="logo"><a href="#"><img src="img/miniicon.png" alt="Sedna logo"></a></div>
+                    <div class="header-nav">
+                        <nav>
+                          <ul class="primary-nav">
+                              <li><a href="#" color:black>Future Career</a></li>
+                              <li><a href="#">Daily Life Sugestion</a></li>
+                              <li><a href="#">Magic 8 Ball</a></li>
+                          </ul>
+                          <ul class="member-actions">
+                                <li><a href="index.html" class="btn-white btn-small">Guest User</a></li>
+                          </ul>
+                        </nav>
+                    </div>
+                    <div class="navicon">
+                        <a class="nav-toggle" href="#"><span></span></a>
+                    </div>
+                </div>
+            </header>       
+        </section>
+    </section>
+
+    <?php
+    mysql_connect("127.0.0.1","root","");
+    mysql_select_db("tellmeyours");
+    $strSQL = "SELECT * FROM tb_result WHERE cooking = '".mysql_real_escape_string($_POST['q1'])."' 
+    and meat = '".mysql_real_escape_string($_POST['q2'])."'";
+    $objQuery = mysql_query($strSQL);
+    $objResult = mysql_fetch_array($objQuery);
+    if(!$objResult)
+    {
+            echo "Error !";
+    }
+    else
+    {
+            $_SESSION["cooking"] = $objResult["cooking"];
+            $_SESSION["meat"] = $objResult["meat"];
+            $_SESSION["menu"] = $objResult["menu"];
+            session_write_close();
+            header("location:daily_result.php");      
+    }
+    mysqli_close();
+?>
+
+    
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="bower_components/retina.js/dist/retina.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="bower_components/classie/classie.js"></script>
+    <script src="bower_components/jquery-waypoints/lib/jquery.waypoints.min.js"></script>
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+</body>
+</html>
